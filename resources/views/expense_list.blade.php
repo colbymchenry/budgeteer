8053ced445b5a22cff783ea505512b081e2e2a36
Colby McHenry <colbymchenry@Colbys-MBP.attlocal.net>
2020-04-23 17:53:44 -0400

    Display total for category in month.

--- resources/views/expense_list.blade.php
+++ resources/views/expense_list.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Expenses for {{ $category->name }}</div>
+                <div class="card-header">Expenses for {{ $category->name }}: ${{ $total }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
